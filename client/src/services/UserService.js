3bfcda159426e2af4ce6bdbe91cf6ace531a2452
Jackie Cheung <jackcheung56@gmail.com>
2020-12-05 16:47:04 -0500

    create services for borrow, item, user

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -18,3 +18,21 @@ export const _GetUser = async (user_id) => {
     throw error
   }
 }
+
+export const _UpdateUser = async (formData, user_id) => {
+  try{
+    const response = await ApiClient.put(`/users/update/${user_id}`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const _DeleteUser = async (user_id) => {
+  try{
+    const response = await ApiClient.delete(`/users/delete/${user_id}`)
+    return response
+  } catch (error) {
+    throw error
+  }
+}
