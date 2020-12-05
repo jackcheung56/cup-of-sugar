e0d9135f9ab1c4c89fa3546772abc230b30488f7
whlong1 <whunterlong@gmail.com>
2020-12-04 20:33:12 -0500

    Server running, database created

---
+++ routes/AppRouter.js
@@ -0,0 +1,4 @@
+const Router = require('express').Router()
+const UserRouter = require('./UserRouter')
+Router.use('/users', UserRouter)
+module.exports = Router
---
+++ routes/UserRouter.js
@@ -0,0 +1,9 @@
+const Router = require('express').Router()
+const controller = require('../controllers/UserController')
+
+//Routes go here
+
+
+
+
+module.exports = Router
