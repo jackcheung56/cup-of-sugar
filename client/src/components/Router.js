6fe0b481bd1fdaafa7ff1feb8c5b0a21a4d98f1b
lewist13 <lewistpro@gmail.com>
2020-12-05 15:48:52 -0500

    built signin, it's displaying

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Navbar from './NavBar'
-import Home from '../pages/Home'
+import React, { useState, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./NavBar";
+import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
 
 function Router() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-
-    return (
-        <div>
-            <Navbar></Navbar>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path="/signin">
+          <SignIn
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+          ></SignIn>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
-export default Router
+export default Router;
