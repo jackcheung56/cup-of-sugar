21d8ee204abcd54defe88ad59c75ee1f048b0f0f
jackcheung56 <71258432+jackcheung56@users.noreply.github.com>
2020-12-05 13:57:34 -0500

    Update UserService.js

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -2,7 +2,7 @@ import ApiClient from "./ApiClient";
 
 export const _CreateUser = async (formData) => {
   try {
-    const response = await ApiClient.post("/user/create", formData);
+    const response = await ApiClient.post("/users/add", formData);
     console.log("USER HERE", response.data);
     return response.data;
   } catch (error) {
