06fb62e39d4f79e719ba34e43765f81b9f76509b
lewist13 <lewistpro@gmail.com>
2020-12-07 11:16:37 -0500

    added auth to approuter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,17 +1,13 @@
-const Router = require('express').Router()
+const Router = require("express").Router();
+const Auth = require("./AuthRouter");
 
+const UserRouter = require("./UserRouter");
+const ItemRouter = require("./ItemRouter");
+const BorrowRouter = require("./BorrowRouter");
 
+Router.use("/auth", AuthRouter);
+Router.use("/users", UserRouter);
+Router.use("/items", ItemRouter);
+Router.use("/borrows", BorrowRouter);
 
-const UserRouter = require('./UserRouter')
-const ItemRouter = require('./ItemRouter')
-const BorrowRouter = require('./BorrowRouter')
-
-
-
-Router.use('/users', UserRouter)
-Router.use('/items', ItemRouter)
-Router.use('/borrows', BorrowRouter)
-
-
-
-module.exports = Router
+module.exports = Router;
