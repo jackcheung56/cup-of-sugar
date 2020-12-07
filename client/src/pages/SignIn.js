1f1b497cfbc0613ccd3c382259b1cae03f016dd2
lewist13 <lewistpro@gmail.com>
2020-12-07 12:16:34 -0500

    prepping signin for auth

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,48 +1,61 @@
 import React, { useState } from "react";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 // import { __GetUser } from '../services/UserService'
 
 const SignIn = ({ user, setUser }) => {
-  console.log('Sign In Props', user)
+  console.log("Sign In Props", user);
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const history = useHistory()
+  const history = useHistory();
 
   const emailInput = (event) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
 
   const passwordInput = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
 
   const loginHandler = (event) => {
-    event.preventDefault()
-    setUser({
-      email: { email },
-      password: { password },
-    })
-    console.log('login test', user)
-    setEmail('')
-    setPassword('')
-    // history.push(`/users/${user.id}`, user={user})
-    history.push('/users/:user_id')
-  }
-
+    event.preventDefault();
+    try {
+      setUser({
+        email: { email },
+        password: { password },
+      });
+      console.log("login test", user);
+      setEmail("");
+      setPassword("");
+      // history.push(`/users/${user.id}`, user={user})
+      toggleAuthenticated(true, setUser.user, () =>
+        history.push("/users/:user_id")
+      );
+    } catch (error) {
+      throw error;
+    }
+  };
 
   return (
     <form>
       <div className="block">
-        <input value={email} onChange={emailInput} type="text"></input>
-        <input value={password} onChange={passwordInput} type="text"></input>
+        <input
+          placeholder="Enter Email"
+          value={email}
+          onChange={emailInput}
+          type="text"
+        ></input>
+        <input
+          placeholder="Enter Password"
+          value={password}
+          onChange={passwordInput}
+          type="text"
+        ></input>
         <button onClick={loginHandler}>Login</button>
       </div>
     </form>
+  );
+};
 
-  )
-}
-
-export default SignIn
+export default SignIn;
