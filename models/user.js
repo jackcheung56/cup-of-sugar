9f8ab738f807122fea177ddbaf84fcda5587d7de
lewist13 <lewistpro@gmail.com>
2020-12-07 11:14:59 -0500

    updated user model w/auth

--- models/user.js
+++ models/user.js
@@ -1,44 +1,54 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-
     static associate(models) {
       // define association here
 
       User.hasMany(models.Item, {
-        foreignKey: 'owner_id',
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-      })
+        foreignKey: "owner_id",
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      });
 
-      User.belongsToMany(models.Item,{
-        as: 'b',
+      User.belongsToMany(models.Item, {
+        as: "b",
 
         through: models.Borrow,
-        foreignKey:'item_id',
+        foreignKey: "item_id",
 
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-      })
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      });
       // end association here
     }
-  };
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    passwordDigest: {
-      type: DataTypes.STRING,
-      field: 'password_digest'
+  }
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      passwordDigest: {
+        type: DataTypes.STRING,
+        field: "password_digest",
+        allowNull: false,
+      },
+      area: DataTypes.INTEGER,
+      rating: DataTypes.INTEGER,
     },
-    area: DataTypes.INTEGER,
-    rating: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'users'
-  });
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
+    }
+  );
   return User;
 };
