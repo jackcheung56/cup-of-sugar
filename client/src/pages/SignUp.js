4991790694ec4034cd90f3e30958cfe67b6d23f1
lewist13 <lewistpro@gmail.com>
2020-12-07 23:17:42 -0500

    jackie need changes

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { __CreateUser } from "../services/UserService";
-
+//pushing for Jackie
 function SignUp(props) {
   console.log(props);
   const [name, setName] = useState("");
