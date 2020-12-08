5eea0e7c2242e9a14492c2e46e32d026c5f61801
lewist13 <lewistpro@gmail.com>
2020-12-08 14:03:50 -0500

    installed dependencies/updated server for socketio

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const logger = require("morgan");
 const AppRouter = require("./routes/AppRouter");
+const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 3001;
 
+io.on("connection", (socket) => {
+  socket.on("message", ({ name, message }) => {
+    io.emit("message", { name, message });
+  });
+});
 app.use(logger("dev"));
 app.use(cors());
 app.use(bodyParser.json());
