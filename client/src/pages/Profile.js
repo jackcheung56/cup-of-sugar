47f4816e54c7bd4534801d41a1d3920269c2d102
whlong1 <whunterlong@gmail.com>
2020-12-10 22:09:10 -0500

    css styling

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -64,6 +64,7 @@ function Profile(props) {
     try {
       const data = await __GetItemByOwner(sortingId)
       setUserItems(data.data)
+      console.log(data.data)
     } catch (error) {
       console.log(error)
     }
