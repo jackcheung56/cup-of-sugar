8a651f5ee1d8f52655bb815da6dd3970e9117fbb
Jackie Cheung <jackcheung56@gmail.com>
2020-12-11 16:36:12 -0500

    trying deployment

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use("/api", AppRouter);
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 )
-app.get("/", (req, res) => res.json({ message: "Server Works" }));
-app.use("/api", AppRouter);
 http.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
